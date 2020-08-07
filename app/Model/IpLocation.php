6b2c9362ff7c0334e39d153b5f3c084f2acab634
Alvin <aepacot2016@gmail.com>
2020-08-08 01:19:38 +0800

    Initial commit

---
+++ app/Model/IpLocation.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * IpLocation class.
+ *
+ * Model class for IP Locations
+ *
+ * LICENSE: This product includes software developed at
+ * the Acelle Co., Ltd. (http://acellemail.com/).
+ *
+ * @category   MVC Model
+ *
+ * @author     N. Pham <n.pham@acellemail.com>
+ * @author     L. Pham <l.pham@acellemail.com>
+ * @copyright  Acelle Co., Ltd
+ * @license    Acelle Co., Ltd
+ *
+ * @version    1.0
+ *
+ * @link       http://acellemail.com
+ */
+
+namespace Acelle\Model;
+
+use App;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log as LaravelLog;
+use Acelle\Library\Notification\BackendError;
+use Acelle\Library\Notification\GeoIp as GeoIpNotification;
+
+class IpLocation extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'country_code', 'country_name', 'region_code',
+        'region_name', 'city', 'zipcode',
+        'latitude', 'longitude', 'metro_code', 'areacode',
+    ];
+
+    /**
+     * Add new IP.
+     *
+     * return Location
+     */
+    public static function add($ip)
+    {
+        //SELECT * FROM `ip2location_db11` WHERE INET_ATON('116.109.245.204') <= ip_to LIMIT 1
+        $location = self::firstOrNew(['ip_address' => $ip]);
+        $geoip = App::make('Acelle\Library\Contracts\GeoIpInterface');
+
+        try {
+            $geoip->resolveIp($ip);
+            GeoIpNotification::cleanupSimilarNotifications();
+        } catch (\Exception $e) {
+            // Note log
+            $title = 'GeoIP Error';
+            LaravelLog::warning('Cannot get IP location info for '.$ip.'. Error: '.$e->getMessage());
+
+            GeoIpNotification::warning([
+                'title' => 'GeoIP error',
+                'message' => $e->getMessage(),
+            ]);
+
+            $geoip->resolveIp('0.0.0.0'); // to safely generate location info
+        }
+
+        $location->ip_address = $ip;
+        $location->country_code = $geoip->getCountryCode();
+        $location->country_name = $geoip->getCountryName();
+        $location->region_name = $geoip->getRegionName();
+        $location->city = $geoip->getCity();
+        $location->zipcode = $geoip->getZipcode();
+        $location->latitude = $geoip->getLatitude();
+        $location->longitude = $geoip->getLongitude();
+        $location->save();
+
+        return $location;
+    }
+
+    /**
+     * Location name.
+     *
+     * return Location
+     */
+    public function name()
+    {
+        $str = [];
+        if (!empty($this->city)) {
+            $str[] = $this->city;
+        }
+        if (!empty($this->region_name)) {
+            $str[] = $this->region_name;
+        }
+        if (!empty($this->country_name)) {
+            $str[] = $this->country_name;
+        }
+        $name = implode(', ', $str);
+
+        return empty($name) ? trans('messages.unknown') : $name;
+    }
+}
---
+++ upgrade/README.txt
@@ -0,0 +1,11 @@
+Please perform upgrade in the order below,
+starting from the nearest next version to your current version
+
+4.0.9.bin
+4.0.19-p61.bin
+4.0.19-p62.bin
+4.0.20-p44.bin
+
+For example:
+If you are already on 4.0.19-p20,
+then start by upgrading to 4.0.19-p61, then 4.0.19-p62 and finally 4.0.20-p44
